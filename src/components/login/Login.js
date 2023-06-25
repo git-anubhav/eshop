cad3f55f7000a7728b8aa063cfc9959e933efccf
git-anubhav <anubhxv@gmail.com>
2023-06-25 11:30:08 +0530

    fix login and signup layout tweaks

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,41 +4,49 @@ import Copyright from '../copyright/Copyright';
 
 export default function Login() {
   return (
-    <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={5}>
+    <Box display={'flex'} justifyContent={'center'} pt={5}>
       <Box
-        backgroundColor={'secondary.main'}
-        p={1}
-        borderRadius={'50%'}
         display={'flex'}
-        justifyContent={'center'}
+        flexDirection={'column'}
         alignItems={'center'}
+        mt={5}
+        maxWidth={'20rem'}
       >
-        <LockOutlinedIcon color='white' />
+        <Box
+          backgroundColor={'secondary.main'}
+          p={1}
+          borderRadius={'50%'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <LockOutlinedIcon color='white' />
+        </Box>
+        <Typography variant='h5' m={3}>
+          Sign in
+        </Typography>
+        <TextField
+          required
+          id='email'
+          label='Email'
+          sx={{ width: '20rem', marginBottom: '1.5rem' }}
+        />
+        <TextField
+          required
+          id='password'
+          label='Password'
+          type='password'
+          sx={{ width: '20rem', marginBottom: '2rem' }}
+        />
+        <Button fullWidth variant='contained'>
+          SIGN IN
+        </Button>
+        <Typography variant='subtitle2' mt={3}>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a href='#'>Don't have an account? Sign Up</a>
+        </Typography>
+        <Copyright />
       </Box>
-      <Typography variant='h5' m={3}>
-        Sign in
-      </Typography>
-      <TextField
-        required
-        id='email'
-        label='Email'
-        sx={{ width: '20rem', marginBottom: '1.5rem' }}
-      />
-      <TextField
-        required
-        id='password'
-        label='Password'
-        type='password'
-        sx={{ width: '20rem', marginBottom: '2rem' }}
-      />
-      <Button fullWidth variant='contained'>
-        SIGN IN
-      </Button>
-      <Typography variant='subtitle2' mt={3}>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href='#'>Don't have an account? Sign Up</a>
-      </Typography>
-      <Copyright />
     </Box>
   );
 }
